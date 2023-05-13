1101a83e95bb8677d363d691ff81472a2734aa8c
Abhimanyu Rana <abhimanyu.rana@postman.com>
2023-05-13 16:54:28 +0530

    polishing

--- src/layouts/theme.js
+++ src/layouts/theme.js
@@ -36,20 +36,15 @@ const theme = {
   value: getColorPreference(),
 }
 
-// set early so no page flashes / CSS is made aware
 reflectPreference()
 
 window.onload = () => {
-  // set on load so screen readers can see latest value on the button
   reflectPreference()
-
-  // now this script can find and listen for clicks on the control
   document
     .querySelector('#theme-toggle')
     .addEventListener('click', onClick)
 }
 
-// sync with system changes
 window
   .matchMedia('(prefers-color-scheme: dark)')
   .addEventListener('change', ({matches:isDark}) => {
