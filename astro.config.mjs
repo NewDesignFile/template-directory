8bbaa251b9f8e97d912702d300be15d11f53beaa
Steve Paul <steveebenezer@protonmail.com>
2023-12-20 00:20:49 +0530

    Add cloudflare adapter to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify/functions";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: cloudflare(),
   integrations: [react()]
 });
