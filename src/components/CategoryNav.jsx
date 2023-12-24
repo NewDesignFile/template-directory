4531d0e7f2c8b50a9696cf3a2a6e3aeb3d003612
planetabhi <abhimanyu.rana@postman.com>
2023-12-24 17:23:10 +0530

    polishing

--- src/components/CategoryNav.jsx
+++ src/components/CategoryNav.jsx
@@ -5,7 +5,7 @@ import CategoryNavItem from "./CategoryNavItem";
 
 export default function CategoryNav({ filter, setFilter }) {
   const navItems = [
-    {title: "&#129303; All", category: "all"}, 
+    {title: "&#129303; All Tools", category: "all"}, 
     ...data.tools
   ];
 
