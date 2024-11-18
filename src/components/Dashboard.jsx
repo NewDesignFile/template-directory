b5830d85a57ea694dc951c7750ce1d261dfa99a7
Caleb Joshua <calebjoshuapaul@gmail.com>
2024-11-19 04:32:15 +0530

    feat: implement category-based filtering in Dashboard and add dynamic category pages

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
 import CategoryNav from "./CategoryNav";
 import CardsContainer from "./CardsContainer";
 
-export default function Dashboard() {
-
-  const [filter, setFilter] = useState("all");
-
-  return <>
-    <CategoryNav filter={filter} setFilter={setFilter} />
-    <CardsContainer filter={filter} />
-  </>
+export default function Dashboard({ category }) {
+	return (
+		<>
+			<CategoryNav filter={category} />
+			<CardsContainer filter={category} />
+		</>
+	);
 }
