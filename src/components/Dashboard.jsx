a07d1c7b206904c2ed9a73d18a72ba16a92f58cd
Steve Paul <steveebenezer@protonmail.com>
2024-11-19 23:55:11 +0530

    Update transitions to persist theme color smoothly

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,18 @@
+import { useEffect, useState } from "react";
 import CategoryNav from "./CategoryNav";
 import CardsContainer from "./CardsContainer";
 
 export default function Dashboard({ category }) {
-	return (
-		<>
-			<CategoryNav filter={category} />
-			<CardsContainer filter={category} />
-		</>
-	);
+  const [currentCategory, setCurrentCategory] = useState(category);
+
+  useEffect(() => {
+    setCurrentCategory(category);
+  }, [category]);
+
+  return (
+    <>
+      <CategoryNav filter={category} />
+      <CardsContainer filter={category} />
+    </>
+  );
 }
