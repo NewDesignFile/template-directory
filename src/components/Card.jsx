3a47ecbb9632012f136dfb2e410b61baa686bfb1
Steve Paul <steveebenezer@protonmail.com>
2024-11-22 15:39:54 +0530

    Add date meta to json and new tag to ui

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,18 @@
 import "./Card.css";
 
 export default function Card(props) {
-  const { href, title, body, tag } = props;
+  const { href, title, body, tag, dateAdded } = props;
+
+  const isNew = () => {
+    if (!dateAdded) return false;
+    
+    const addedDate = new Date(dateAdded);
+    const today = new Date();
+    const differenceInTime = today.getTime() - addedDate.getTime();
+    const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+    
+    return differenceInDays <= 30;
+  };
 
   return (
     <li className="link-card">
@@ -9,6 +20,7 @@ export default function Card(props) {
         <strong className="nu-c-h6 nu-u-mt-1 nu-u-mb-1">{title}</strong>
         <p className="nu-c-fs-small nu-u-mt-1 nu-u-mb-1">{body}</p>
         <p className="distribution">
+          {isNew() && <span className="tag nu-u-me-2">New</span>}
           <span className="tag">{tag}</span>
         </p>
       </a>
