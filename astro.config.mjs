320655d46ec6e2474f7e9e177a9ab1f3cb892094
planetabhi <theplanetabhi@gmail.com>
2024-12-16 14:25:30 +0530

    added Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
 
+import partytown from "@astrojs/partytown";
+
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(), 
+    partytown(
+      {
+        config: {
+          forward: ["dataLayer.push"],
+        },
+      }
+    )],
 
   output: "server",
   adapter: netlify()
